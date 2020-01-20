35753d122dc4c8680e7a6cf28584dabe084321c5
Sergio <Email>
2020-01-20 16:17:09 +0100

    Correo en Tabla

---
+++ src/app/Components/nuevo-correo/nuevo-correo.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
+@Component({
+  selector: 'app-nuevo-correo',
+  templateUrl: './nuevo-correo.component.html',
+  styleUrls: ['./nuevo-correo.component.scss']
+})
+export class NuevoCorreoComponent implements OnInit {
+
+  nuevoCorreo: FormGroup;
+  submitted = false;
+
+  // Este es el nombre que se le da entre [] <app-nuevo-correo [correo]="correo"></app-nuevo-correo>
+  @Input() correo: any; 
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+      this.nuevoCorreo = this.formBuilder.group({
+        titulo: ['', [Validators.required, Validators.minLength(3)]],
+        cuerpo: ['', [Validators.required, Validators.minLength(10)]],
+        destinatario: ['', [Validators.required, Validators.email]],
+      });
+      
+      if(this.correo != undefined){
+        console.log("A",this.correo);
+        this.nuevoCorreo.patchValue({
+          titulo: 'Re: '+ this.correo.titulo, 
+          destinatario: this.correo.destinatario
+        });
+      }
+  }
+
+  get formulario() { return this.nuevoCorreo.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      if (this.nuevoCorreo.invalid) {
+          return;
+      }
+
+      let correo = this.nuevoCorreo.value;
+      correo.leido= false;
+      correo.emisor= 'correoEmisor1@openWebinar.inv';
+
+      alert("Correo Enviado \nEliminamos el formulario");
+      this.onReset();
+  }
+
+  onReset() {
+      this.submitted = false;
+      this.nuevoCorreo.reset();
+  }
+
+}
